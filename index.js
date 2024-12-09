e95c4625a1b62969325b18a6b21f2aa987f3b3c8
Temitope Odeniran <odenirantemitope@gmail.com>
2024-12-09 07:24:59 +0100

    javascript functions

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 //JAVASCRIPT FUNCTION
+
+// function greet(name){
+//     console.log("Good morning " + name)
+// }
+
+// greet("Temitope")
+
+// //A function that calculate the area of a rectangle
+
+// function calculateArea(width,height){
+//     return width * height
+// }
+// console.log(calculateArea(5,10))
+
+
+// function areaTriangle(base,height){
+//     return 1/2 * base * height
+// }
+// console.log(areaTriangle(3,9))
+
+// //ARROW FUNCTION
+// const sum = (a,b) => {
+//     return a+b
+// }
+// console.log(sum(45,50))
+
+//A function that calculate simple interest
+
+//Temitope deposited $250 in asavings account offering an interest rate of 7.5%. If he keeps the money in the account for 5years how much will he earn?
+function simpleInterest(principal,rate,time){
+    return 1/100 * principal * rate * time
+}
+console.log(simpleInterest(250,75,5))
+
+
+
+  
